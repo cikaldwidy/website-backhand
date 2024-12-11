38cd015deb69e441dfc8218c585d9eb8b21a064e
Alica <alicaputriazz@gmail.com>
2024-12-11 21:49:32 +0700

    feat: adding wa contact and program into db

--- kelompok13/funeducare/users/models.py
+++ kelompok13/funeducare/users/models.py
@@ -37,9 +37,9 @@ class CustomUser(AbstractUser):
         ],
     )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # Gunakan email sebagai field login utama
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []  
+    # # Gunakan email sebagai field login utama
+    # USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = []  
 
     def __str__(self):
         return self.email
